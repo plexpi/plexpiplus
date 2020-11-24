e4db2aa5842d2b4e0d50cbe07dc274aaad8c784c
Adam Borbas <adam.borbas@bitrise.io>
2020-11-24 08:20:18 +0100

    Clear unused code

--- PIPlus/PIPlus/MainModule/MainModuleResolver.swift
+++ PIPlus/PIPlus/MainModule/MainModuleResolver.swift
@@ -10,18 +10,7 @@ import TorrentSearch
 import TorrentDownloader
 
 class MainModuleResolver: MainModule {
-    func resolveFilterViewPresenter(view: OFilterView) -> FilterViewPresenter {
-        return FilterViewPresenter(view: view, filterManager: resolveFilterManager())
-    }
-    
     static let shared = MainModuleResolver()
-    
-    func resolveMainViewPresenter(view: MainView) -> MainViewPresenter {
-        return MainViewPresenter(view: view,
-                                 torrentSearcher: resolveTorrentSearcher(),
-                                 torrentDownloader: resolveTorrentDownloader(),
-                                 filterManager: resolveFilterManager())
-    }
     
     private func resolveTorrentSearcher() -> TorrentSearcher {
         return PITorrentSearcher(baseURL: URL(string: "http://raspberrypi.local:5000")!)
